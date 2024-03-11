02cfb5c8fd7c59023802dcb13fbbf2421cf23de2
hwlee9905 <9905hyun@naver.com>
2024-03-12 02:39:18 +0900

    [Test] DataSource Test 추가

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,8 +1,11 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,6 +16,29 @@ import static hello.jdbc.connection.ConnectionConst.*;
 public class ConnectionTest {
     @Test
     void driverManager() throws SQLException {
+        Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", connection1, connection1.getClass());
+        log.info("connection={}, class={}", connection2, connection2.getClass());
+    }
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        //DriverManagerDataSource - 항상 새로운 커넥션을 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource hikariDataSource = new HikariDataSource();
+        hikariDataSource.setJdbcUrl(URL);
+        hikariDataSource.setUsername(USERNAME);
+        hikariDataSource.setUsername(PASSWORD);
+        hikariDataSource.setMaximumPoolSize(10);
+        hikariDataSource.setPoolName("MyPool");
+        useDataSource(hikariDataSource);
+        Thread.sleep(1000);
+    }
+    private void useDataSource(DataSource dataSource) throws SQLException {
         Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection connection2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         log.info("connection={}, class={}", connection1, connection1.getClass());
