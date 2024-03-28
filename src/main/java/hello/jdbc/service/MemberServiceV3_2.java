0a027b4f63c8e03ff18fe61bf11cf7ff6a75d8e5
hwlee9905 <9905hyun@naver.com>
2024-03-28 22:27:17 +0900

    [Fix] 공백 제거

--- src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -19,12 +19,10 @@ import java.sql.SQLException;
 public class MemberServiceV3_2 {
     private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository;
-
     public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
         this.txTemplate = new TransactionTemplate(transactionManager);
         this.memberRepository = memberRepository;
     }
-
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
         txTemplate.executeWithoutResult((status) -> {
             try {
@@ -34,7 +32,6 @@ public class MemberServiceV3_2 {
             }
         });
     }
-
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
@@ -43,8 +40,6 @@ public class MemberServiceV3_2 {
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
     }
-
-
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체중 예외 발생");
