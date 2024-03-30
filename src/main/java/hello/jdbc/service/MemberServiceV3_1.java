8d70da1b0b737ddf98fce16576d5b399c9fb69b7
hwlee9905 <9905hyun@naver.com>
2024-03-30 23:36:26 +0900

    [Fix]

--- src/main/java/hello/jdbc/service/MemberServiceV3_1.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -36,7 +36,6 @@ public class MemberServiceV3_1 {
             throw new IllegalStateException(e);
         }
     }
-
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
         Member fromMember = memberRepository.findById(fromId);
         Member toMember = memberRepository.findById(toId);
@@ -45,7 +44,6 @@ public class MemberServiceV3_1 {
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
     }
-
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체중 예외 발생");
