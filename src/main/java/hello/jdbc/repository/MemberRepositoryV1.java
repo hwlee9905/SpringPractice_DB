f63db256097be5354065f8ff4915bdc391a9d22e
hwlee9905 <9905hyun@naver.com>
2024-04-10 01:53:13 +0900

    [Fix]

--- src/main/java/hello/jdbc/repository/MemberRepositoryV1.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV1.java
@@ -18,7 +18,6 @@ public class MemberRepositoryV1 {
     private final DataSource dataSource;
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values (?, ?)";
-
         Connection con = null;
         PreparedStatement pstmt = null;
         try {
